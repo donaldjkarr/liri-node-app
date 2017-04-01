c0e40c18c380adb7964a8eb28dd19dd959d39cb1
donaldjkarr <donaldjkarr@gmail.com>
2017-04-01 09:08:13 -0500

    Delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('loading...');
-
-exports.twitterKeys = {
-  consumer_key: 'FkdoVrgFfx3KUlVkHjdDVVNxU',
-  consumer_secret: 'xt1dEwXKlZZ6wm1XqkOfJlJKQiXXb1On3xR06sPyQFMuvt2t3R',
-  access_token_key: '847948226674274304-lXq8qybdhgkLC7NZ3IEn1PsMmaiWHw0',
-  access_token_secret: 'ptZVOI6T3zAnkT21FnNru0Vkxy5fRSBpD17RW1YgzplL1',
-}
